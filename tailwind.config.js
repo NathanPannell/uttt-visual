7e8ceee24241a10279d97f0bde2d6b194f575615
Nathan Pannell <n8tewolf@gmail.com>
2023-11-17 00:35:55 -0800

    fix borders on vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
   purge: {
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     options: {
-      safelist: ["border-r-black", "border-r-transparent" /* other classes */],
+      safelist: [
+        "border-r-black",
+        "border-r-transparent",
+        "border-t-black",
+        "border-t-transparent",
+        "border-b-black",
+        "border-b-transparent",
+        "border-l-black",
+        "border-l-transparent",
+      ],
     },
   },
 };
